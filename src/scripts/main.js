2aa15e97b3b8372b2964242fb734b37c8f17dd6e
M <marilynduf@gmail.com>
2025-10-31 17:27:44 -0400

    Refactor logic + eval()

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,67 +24,77 @@ const isLastOperator = (val) => isOperator(val[val.length - 1]);
 function displayInputs(expression) {
     screen.textContent += expression;
 }
-function effaceDerniereToucheEntree() {
+function RemoveLastKeyEntered() {
     const screenValue = screen.textContent;
     screen.textContent = screenValue.slice(0, -1);
 }
 
 function handleUserInput(userInput) {
     const content = screen.textContent;
-    if (isNumber(userInput) && content.length <= 1 && content === "0") {
-        screen.textContent = "";
-        displayInputs(userInput);
+    // si input est un chiffre ET que la  moin ET que la valeur du seul, on ajoute le input
+    /**
+     * Logique métier :
+     * - Si input est un chiffre ET
+     * - Si longueur chaîne est égale à 1 ou moins
+     * - Si content est égal à 0
+     * - Résultat → Affiche userInput
+     */
+
+    if (isNumber(userInput)) {
+        if (content.length <= 1 && content === "0") {
+            screen.textContent = "";
+            displayInputs(userInput);
+            return;
+        } else displayInputs(userInput);
         return;
     }
 
-    if (isOperator(userInput) && isLastOperator(content)) {
-        if (content[content.length - 1] === userInput) return;
-        else {
-            effaceDerniereToucheEntree();
-        }
-    }
-
-    if (isNumber(userInput) || isOperator(userInput)) {
-        displayInputs(userInput);
+    if (isOperator(userInput)) {
+        if (isLastOperator(content)) {
+            const lastCaracter = content[content.length - 1];
+            if (lastCaracter === userInput) {
+                return;
+            } else {
+                RemoveLastKeyEntered();
+                displayInputs(userInput);
+            }
+        } else displayInputs(userInput);
         return;
     }
+
     if (userInput === "=") {
+        // Si le dernier caractere de l'expression est un opérateur, on ne fait rien
         if (isLastOperator(content)) return;
-        // si expression ne contient pas d'operateir -> return
+        // Si l'expression contient un opérateur, on affiche l'espression dans le petit écran puis on évalue l'expression, si non, on ne fait rien
         if (/[/*+\-]/.test(content)) {
             littleScreen.textContent = content;
+            const resultat = eval(content);
+            screen.textContent = eval(resultat);
         } else return;
     }
 
-    if (userInput === "BS") {
+    if (userInput === "Backspace") {
         if (content.length <= 1) {
             screen.textContent = "0";
-        } else effaceDerniereToucheEntree();
+        } else RemoveLastKeyEntered();
+        return;
     }
     if (userInput === "effacer") {
         screen.textContent = "0";
         littleScreen.textContent = "";
+        return;
     }
-}
-
-function inputKeyNormalization(key) {
-    if (key === "Enter") {
-        return "=";
-    }
-    if (key === "Backspace") {
-        return "BS";
-    } else return key;
 }
 
 function isAllowedKeys(key) {
     const allowedKeys = "0123456789+-*/=";
     const isKeyAllowed = allowedKeys.includes(key);
-    if (isKeyAllowed || key === "=" || key === "BS") {
+    if (isKeyAllowed || key === "=" || key === "Backspace") {
         return true;
     } else return false;
 }
 window.addEventListener("keydown", (e) => {
-    const key = inputKeyNormalization(e.key);
+    const key = e.key === "Enter" ? "=" : e.key; // Standardise la touche "=" et "Enter"
     if (!isAllowedKeys(key)) return;
     if (e.repeat) return; // Empeche la touche de rester "stick" sur le clavier
     const button = document.querySelector(`[data-val="${key}"]`);
@@ -93,7 +103,7 @@ window.addEventListener("keydown", (e) => {
 });
 
 window.addEventListener("keyup", (e) => {
-    const key = inputKeyNormalization(e.key);
+    const key = e.key === "Enter" ? "=" : e.key;
     if (!isAllowedKeys(key)) return;
     if (e.repeat) return; //Empeche la touche de rester "stick" sur le clavier
     const button = document.querySelector(`[data-val="${key}"]`);
@@ -102,6 +112,7 @@ window.addEventListener("keyup", (e) => {
 
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
+        console.log(button.dataset.val);
         handleUserInput(button.dataset.val);
     });
 });
